27e0f61397bcf494d58cc67027f0ec82115e1307
shane1027 <sryan8@nd.edu>
2016-06-09 12:12:12 -0500

    hello, world

---
+++ Chapter_2/hello_world.cpp
@@ -0,0 +1,15 @@
+/******** From Section 2.2.1 ********
+ * Author:  Shane Ryan
+ * Name: hello_world.cpp
+ * Purpose: say hello
+ ***********************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    cout << "hello, world!\n";
+
+}
